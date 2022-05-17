82dc804964d22b3404111c3eb4aa50acba1d46e8
joaocorner <joaoeduardocorner@gmail.com>
2022-05-16 23:49:42 -0300

    Criando meu primeiro arquivo .NET

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numeroDeVezes = 10;
+for (int i = 0; i < numeroDeVezes; i++)
+{
+    Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+}
